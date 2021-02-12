5b900816e8ddc6882d2dbba3a964395a5d8bcb28
Valen <valentinodidio943@gmail.com>
2021-02-12 12:55:39 -0300

    Fixed config bits

--- main.c
+++ main.c
@@ -1,14 +1,14 @@
 // CONFIG1
 #pragma config FOSC = INTRC_NOCLKOUT// Oscillator Selection bits (INTOSCIO oscillator: I/O function on RA6/OSC2/CLKOUT pin, I/O function on RA7/OSC1/CLKIN)
 #pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled and can be enabled by SWDTEN bit of the WDTCON register)
-#pragma config PWRTE = ON       // Power-up Timer Enable bit (PWRT enabled)
-#pragma config MCLRE = ON       // RE3/MCLR pin function select bit (RE3/MCLR pin function is MCLR)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config MCLRE = OFF      // RE3/MCLR pin function select bit (RE3/MCLR pin function is digital input, MCLR internally tied to VDD)
 #pragma config CP = OFF         // Code Protection bit (Program memory code protection is disabled)
 #pragma config CPD = OFF        // Data Code Protection bit (Data memory code protection is disabled)
-#pragma config BOREN = ON       // Brown Out Reset Selection bits (BOR enabled)
-#pragma config IESO = ON        // Internal External Switchover bit (Internal/External Switchover mode is enabled)
-#pragma config FCMEN = ON       // Fail-Safe Clock Monitor Enabled bit (Fail-Safe Clock Monitor is enabled)
-#pragma config LVP = OFF        // Low Voltage Programming Enable bit (RB3 pin has digital I/O, HV on MCLR must be used for programming)
+#pragma config BOREN = ON       // Brown Out Reset Selection bits (BOR disabled)
+#pragma config IESO = OFF       // Internal External Switchover bit (Internal/External Switchover mode is disabled)
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enabled bit (Fail-Safe Clock Monitor is disabled)
+#pragma config LVP = ON        // Low Voltage Programming Enable bit (RB3 pin has digital I/O, HV on MCLR must be used for programming)
 
 // CONFIG2
 #pragma config BOR4V = BOR40V   // Brown-out Reset Selection bit (Brown-out Reset set to 4.0V)
@@ -49,54 +49,54 @@ void main(){
     
     while(1)
     {
-        if (RCIF == 1){
+        if (RCIF == 1){ //Recibe comandos del dispositivo maestro
             RX_Byte();
         }
-        switch(modo)
+        switch(modo) //control de relees a R calefactora
         {
-            case 1:
+            case 1: //apagado, 0Â°
                 RA2=0;
                 RA3=0;
                 RA5=0;
                 break;
-            case 2:
+            case 2: // 80Â°
                 RA2=1;
                 RA3=0;
                 RA5=0;
                 break;
-            case 3:
+            case 3: // 90Â°
                 RA2=0;
                 RA3=0;
                 RA5=1;
                 break;
-            case 4:
+            case 4: // 100Â°
                 RA2=0;
                 RA3=1;
                 RA5=0;
                 break;
         }
-        GO_DONE=1; // empieza la conversión
-        __delay_ms(1); // espera el fin de la conversión
-        
+        //MIDE TEMPERATURA
+        GO_DONE=1; // empieza la conversiÃ³n
+        __delay_ms(1); // espera el fin de la conversiÃ³n
         resultado=ADRESH<<8;
         resultado=resultado+ADRESL;
-        
-        if(resultado<163) // 163 representa 80° en decimal
+        //ENCIENDE LEDS DEPENDIENDO DE LA TEMPERATURA
+        if(resultado<163) // 0Â°
         {
             PORTB=0b00000000; 
             PORTA=0b00000000;
         } 
-        else if(resultado<184) //  184 representa 90° en decimal 
+        else if(resultado<184) //  80Â°
         {
             PORTB=0b11110000; 
             PORTA=0b00000000;
         } 
-        else if(resultado<204) // 204 representa 100° en decimal
+        else if(resultado<204) // 90Â°
         {
             PORTB=0b11111100; 
             PORTA=0b00000000;
         } 
-        else if(resultado<225) // 225 representa 110° en decimal
+        else if(resultado>=204) // 100Â°
         {
             PORTB=0b11111111; 
             PORTA=0b11000000;
