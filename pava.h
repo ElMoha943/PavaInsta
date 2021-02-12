32be981cc6de370fbc5ebda4f8c01119a394f700
Valen <valentinodidio943@gmail.com>
2021-02-12 13:06:19 -0300

    Moved _XTAL_FREQ from main
    Moved _XTAL_FREQ from main to pava.h

--- pava.h
+++ pava.h
@@ -1,17 +1,18 @@
-
 #ifndef PAVA_H
 #define	PAVA_H
 
-#include<xc.h> // include processor files - each processor file is guarded.  
+#define _XTAL_FREQ 4000000
 
+//VARIABLES
 unsigned char J[5]="00001", REF1[10]="Disp", REF2[55]="Dispositivo que controla la temperatura de una pava.";
 unsigned char ArrayRX[20], Indice, ArrayProc[20];
 unsigned int resultado=0;
-void SendSerial(unsigned char *data, unsigned char lenght);
-void RX_Byte(void);
-void ProcesarArray(void);
 unsigned char codigo(void);
 unsigned char modo;
 
-#endif	/* PAVA_H */
+//FUNCIONES
+void SendSerial(unsigned char *data, unsigned char lenght);
+void RX_Byte(void);
+void ProcesarArray(void);
 
+#endif	/* PAVA_H */
