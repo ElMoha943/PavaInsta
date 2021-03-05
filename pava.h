636229ed3ab34320d8f2d9fa96e27fac235ef7c5
Valen <valentinodidio943@gmail.com>
2021-03-04 22:13:16 -0300

    Update pava.h

--- pava.h
+++ pava.h
@@ -4,15 +4,15 @@
 #define _XTAL_FREQ 4000000
 
 //VARIABLES
-unsigned char J[5]="00001", REF1[10]="Disp", REF2[55]="Dispositivo que controla la temperatura de una pava.";
+unsigned char J[5]="00001", REF1[10]="Disp", REF2[50]="Dispositivo que controla la temperatura de una pava.";
+unsigned char recibido, comando, command_data[10], command_data_index=0, command;
 unsigned char ArrayRX[20], Indice, ArrayProc[20];
-unsigned int resultado=0;
-unsigned char codigo(void);
 unsigned char modo;
 
 //FUNCIONES
 void SendSerial(unsigned char *data, unsigned char lenght);
 void RX_Byte(void);
 void ProcesarArray(void);
+unsigned char Codigo(void);
 
 #endif	/* PAVA_H */
