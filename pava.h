13ccd42e06370803b7517b2d93ac42d05436706b
Valen <valentinodidio943@gmail.com>
2021-03-10 13:15:22 -0300

    El moha943 patch 1 (#3)
    * Added J jumpers
    
    * Fixed REF2 lenght
    
    * Fixed conflicting variable type
    
    * Updated circuit
    
    + Jumpers
    - Test leds
    
    * Final version compilation.

--- pava.h
+++ pava.h
@@ -4,14 +4,14 @@
 #define _XTAL_FREQ 4000000
 
 //VARIABLES
-unsigned char J[5]="00001", REF1[10]="Disp", REF2[50]="Dispositivo que controla la temperatura de una pava.";
+unsigned char J[]="00000", REF1[10]="Disp", REF2[55]="Dispositivo que controla la temperatura de una pava.";
 unsigned char recibido, comando, command_data[10], command_data_index=0, command;
 unsigned char ArrayRX[20], Indice, ArrayProc[20];
 unsigned char modo;
 int resultado;
 
 //FUNCIONES
-void SendSerial(unsigned char *data, unsigned char lenght);
+void SendSerial(unsigned char *data);
 void RX_Byte(void);
 void ProcesarArray(void);
 unsigned char Codigo(void);
