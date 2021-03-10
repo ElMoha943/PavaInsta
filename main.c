608243a3aeb9c15da7cfc97f154999ca0c623ad9
Valen <valentinodidio943@gmail.com>
2021-03-10 13:09:52 -0300

    Added J jumpers

--- main.c
+++ main.c
@@ -52,6 +52,12 @@ void main(){
     RA4=0;
     RA5=0;
     
+    if(RD0==1) J[0]=1;
+    if(RD1==1) J[1]=1;
+    if(RD2==1) J[2]=1;
+    if(RD3==1) J[3]=1;
+    if(RD4==1) J[4]=1;
+    
     int contador=0;
     while(1){
         //RECIBE UN COMANDO
